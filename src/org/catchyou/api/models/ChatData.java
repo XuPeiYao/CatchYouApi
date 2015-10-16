458382c2fd5f22d24165dc4f50067f1faed75b49
Pei Yao,Xu <u0124008@nkfust.edu.tw>
2015-10-17 01:32:17 +0800

    add method

--- src/org/catchyou/api/models/ChatData.java
+++ src/org/catchyou/api/models/ChatData.java
@@ -5,6 +5,10 @@ import org.json.serialization.JSONSerializable;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.serialization.DeserializeException;
+import org.json.serialization.JSONConvert;
 
 @JSONSerializable
 public class ChatData {
@@ -23,6 +27,15 @@ public class ChatData {
     @JSONProperty
     public long time;
 
+    public UserInfo getRequestInfoContent(){
+        try {
+            return JSONConvert.deserialize(UserInfo.class, content);
+        } catch (DeserializeException ex) {
+            Logger.getLogger(ChatData.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+    
     public String getTimeString() {
         Date time_ = new Date(time);
         return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(time_);
