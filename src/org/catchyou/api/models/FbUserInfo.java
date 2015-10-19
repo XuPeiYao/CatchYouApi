b8ad4dc4ebb43300c62c50ccb293278dfb1fe10c
Pei Yao,Xu <u0124008@nkfust.edu.tw>
2015-10-19 22:16:30 +0800

    add anonymous check method

--- src/org/catchyou/api/models/FbUserInfo.java
+++ src/org/catchyou/api/models/FbUserInfo.java
@@ -33,14 +33,20 @@ public class FbUserInfo {
     @JSONProperty(key = "cover")
     public Cover cover;
 
+    public String getName(){return getName(null);}
     public String getName(String Default) {
         if(name_format == null)return Default;
         return name_format.replaceAll("\\{last\\}", safeString(last_name)).replaceAll("\\{first\\}", safeString(first_name)).replaceAll("\\{middle\\}", safeString(middle_name));
     }
-
+    
+    public Object getPhotoSticker(){return getPhotoSticker(null);}
     public Object getPhotoSticker(Object Default){
         if(id==null)return Default;
         return "https://graph.facebook.com/v2.3/"+ id +"/picture?height=256&width=256";
+    }
+    
+    public boolean isAnonymou(){
+        return getName(null) == null;
     }
     
     private String safeString(String Value) {
